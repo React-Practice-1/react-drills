e54fbfafa613182f6fe9e1877ccb27e463abadc5
Rob-Swensen <rg.swensen801@gmail.com>
2020-02-26 21:19:27 -0700

    Fourth Commit

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -6,13 +6,28 @@ class App extends Component{
     super();
 
     this.state = {
-      array: ['Cookies','Soda', 'Humans', 'Cats', 'Dogs', 'Ice Cream', 'Sushi', 'Bombay House', 'Bomb', 'Countries']
+      array: ['Cookies','Soda', 'Humans', 'Cats', 'Dogs', 'Ice Cream', 'Sushi', 'Bombay House', 'Bomb', 'Countries'],
+      filterString: '',
     }
   }
+
+  handleChange(filter){
+    this.setState({filterString: filter})
+  }
+
+
   render(){
+    let foodsToDisplay = this.state.array
+    .filter((element, index) => {
+      return element.includes(this.state.filterString);
+    })
+    .map((element, index) => {
+      return <h4 key={index}>{element}</h4>;
+    });
     return(
       <div className='App'>
-        <input/>
+        <input onChange={(e) => this.handleChange(e.target.value)}/>
+        {foodsToDisplay}
       </div>
     )
   }
